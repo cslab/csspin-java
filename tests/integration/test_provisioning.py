e336638acfa5eb2aa6f143ae5e8981104c1a99cf
Benjamin Thomas Schwertfeger <benjaminthomas.schwertfeger@contact-software.com>
2024-10-02 07:12:37 +0000

    Resolve "--provision and --cleanup are now tasks"

--- tests/integration/test_provisioning.py
+++ tests/integration/test_provisioning.py
@@ -6,37 +6,41 @@
 
 """Module implementing the integration tests for spin_java"""
 
-from os import getcwd
-from os.path import join
+
+import subprocess
 
 import pytest
-from spin import backtick, cd, cli
 
 
-@pytest.fixture(autouse=True)
-def cfg():
-    """Fixture creating the configuration tree"""
-    cwd = getcwd()
-    cli.load_config_tree("tests/yamls/minimal.yaml")
-    cd(cwd)
-
-
-def execute_spin(tmpdir, what, cmd, path="tests/integration"):
-    """Helper to execute spin calls via spin."""
-    output = backtick(
-        f"spin -p spin.data={tmpdir} -q -C {path} --env {tmpdir} -f"
-        f" {what} --cleanup --provision {cmd}"
-    )
-    output = output.strip()
-    return output
+def execute_spin(yaml, env, path="tests/integration/yamls", cmd=""):
+    """Helper function to execute spin and return the output"""
+    try:
+        return subprocess.check_output(
+            (
+                f"spin -p spin.cache={env} -C {path} --env {str(env)} -f {yaml} " + cmd
+            ).split(" "),
+            encoding="utf-8",
+            stderr=subprocess.PIPE,
+        ).strip()
+    except subprocess.CalledProcessError as ex:
+        print(ex.stdout)
+        print(ex.stderr)
+        raise
 
 
 @pytest.mark.integration()
-def test_java(tmpdir):
-    """The java plugin-package can be provisioned"""
-    output = execute_spin(
-        tmpdir=tmpdir,
-        what=join("tests", "integration", "yamls", "java.yaml"),
-        cmd="run java --version",
-    )
-    assert "openjdk 19." in output
+def test_java_provision(tmp_path):
+    """Provision the java plugin"""
+    yaml = "java.yaml"
+    execute_spin(yaml=yaml, env=tmp_path, cmd="cleanup")
+    execute_spin(yaml=yaml, env=tmp_path, cmd="provision")
+    execute_spin(yaml=yaml, env=tmp_path, cmd="run java --version")
+
+
+@pytest.mark.integration()
+def test_mvn_provision(tmp_path):
+    """Provision the mvn plugin"""
+    yaml = "mvn.yaml"
+    execute_spin(yaml=yaml, env=tmp_path, cmd="cleanup")
+    execute_spin(yaml=yaml, env=tmp_path, cmd="provision")
+    execute_spin(yaml=yaml, env=tmp_path, cmd="run mvn --version")
