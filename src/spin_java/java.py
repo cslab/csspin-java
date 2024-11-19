df85172c4dfca2ed032bdc2dbe0a960288d1510a
Fabian Hafer <fabian.hafer@contact-software.com>
2024-11-19 10:37:06 +0000

    Resolve "Provide a way to reuse an existing java installation"

--- src/spin_java/java.py
+++ src/spin_java/java.py
@@ -8,94 +8,49 @@
 
 import os
 
-from spin import config, die, echo, interpolate1, memoizer, mkdir, setenv
+from path import Path
+from spin import config, die, echo, interpolate1, mv, setenv
 
-defaults = config(
-    version=None,
-    install_dir="{spin.data}",
-    java_home=None,
-)
-
-N = os.path.normcase
+defaults = config(install_dir="{spin.data}/java")
 
 
-def set_java_home(cfg):
-    """Extend the environment variables"""
-    setenv(JAVA_HOME=cfg.java.java_home)
+def set_environment(cfg):
+    """Set the environment variables needed for java"""
+    java_bin_dir = cfg.java.java_home / "bin"
     setenv(
-        N(f"set PATH=$JAVA_HOME/bin{os.pathsep}$PATH"),
-        PATH=os.pathsep.join((N("{JAVA_HOME}/bin"), "{PATH}")),
+        JAVA_HOME=cfg.java.java_home,
+        PATH=os.pathsep.join((java_bin_dir, "{PATH}")),
     )
-
-
-def check_java(cfg):
-    """Checks for a present java installation"""
-    mkdir(cfg.java.install_dir)
-    with memoizer(cfg.java.install_dir / "java.pickle") as m:
-        version = None
-        for version, java_home in m.items():
-            if version == cfg.java.version:
-                cfg.java.java_home = java_home
-                set_java_home(cfg)
-                return True
-    return False
-
-
-def provision(cfg):
-    """Provision the java plugin"""
-    if check_java(cfg):
-        return
-
-    import jdk
-
-    real_get_download_url = jdk.get_download_url
-
-    def monkey_get_download_url(*args):
-        url = real_get_download_url(*args)
-        echo(f"Downloading JDK from {url}")
-        return url
-
-    jdk.get_download_url = monkey_get_download_url
-
-    with memoizer(cfg.java.install_dir / "java.pickle") as m:
-        # In case there already is a JDK installation, but it is not
-        # yet memoized, unpacking the freshly downloaded new
-        # distribution into the same directory will fail, since the
-        # JDK archives have r/o files. But we cannot remove the
-        # installation directory beforehand, since we do not know it's
-        # name: `java_home` is computed from the archive contents.
-        #
-        # FIXME: with some effort, we should be able to come up with a
-        # better solution than to error out and let the user manually
-        # remove the target directory.
-        try:
-            java_home = jdk.install(
-                cfg.java.version, path=interpolate1(cfg.java.install_dir)
-            )
-        except PermissionError as ex:
-            die(
-                "Unpacking the JDK archive failed. There probably already is a JDK in"
-                f" the same location, that you have to remove manually ({ex})."
-            )
-        m.add((cfg.java.version, java_home))
-        cfg.java.java_home = java_home
-
-    set_java_home(cfg)
-
-
-def init(cfg):
-    """Initialize the java plugin"""
-    if not check_java(cfg):
-        die(
-            "JDK {java.version} is not yet provisioned.\n"
-            "You might want to run 'spin provision'."
-        )
 
 
 def configure(cfg):
     """Configure the java plugin"""
-    if not cfg.java.version:
+    if cfg.java.use:
+        cfg.java.java_home = cfg.java.use
+    elif cfg.java.version:
+        cfg.java.java_home = Path(interpolate1(cfg.java.install_dir)) / cfg.java.version
+    else:
         die(
-            "Spin's Java plugin no longer sets a default version.\n"
-            "Please choose a version in spinfile.yaml by setting java.version"
+            "'spin_java.java' does not set a default version for java. "
+            "Set either 'java.version' or 'java.use'."
         )
+
+
+def provision(cfg):
+    """Install java if it does not exist"""
+    if not cfg.java.java_home.exists():
+        from tempfile import TemporaryDirectory
+
+        import jdk
+
+        with TemporaryDirectory() as tmp_dir:
+            echo(f"Downloading JDK from {jdk.get_download_url(cfg.java.version)}")
+            jdk_path = jdk.install(cfg.java.version, path=Path(tmp_dir))
+            mv(jdk_path, cfg.java.java_home)
+
+    set_environment(cfg)
+
+
+def init(cfg):
+    """Initialize the environment"""
+    set_environment(cfg)
